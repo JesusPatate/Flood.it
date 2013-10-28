e61334eb4458457fa82c6f9ee29006dcce930c7c
Stan <stan@stan-laptop.(none)>
2013-10-28 15:39:03 +0100

    insertion lseq ok

--- lseq.js
+++ lseq.js
@@ -1,78 +1,292 @@
-/*********************************************
-Dépendances : 
 
-Interface fournie :
-  - messageDelivered(msg);
-
-   
-Interface requise: 
-
-***********************************************/
-
+/**
+ * \class LSEQNode
+ * \brief Node of an exponential tree
+ */
 function LSEQNode(value){
-  this.value = value;
-  this.children = [];
-  this.next = null; // à voir
+    this.value = value;
+    this.size = 0;
+    this.children = [];
 }
 
 LSEQNode.prototype.toString = function(){
   var repr = this.value && this.value.toString() || '';
   
-  for(var child in this.children){
-    repr += this.children[child].toString();
+  for(var i=0; i<this.children.length; i++){
+      var child = this.children[i];
+      
+      if(child){
+        repr += child.toString();
+      }
   }
   
   return repr;
 };
 
-function LSEQ(){
-  this._root = new LSEQNode();
-  this._root.children[0] = new LSEQNode(null);
+/**
+ * \class LSEQTree
+ * \brief Exponential tree for LSEQ
+ */
+function LSEQTree(base, boundary){
+    const DEFAULT_BASE = 32;
+    const DEFAULT_BOUNDARY = 10;
+    
+    this._base = base || DEFAULT_BASE;
+    this._boundary = boundary || DEFAULT_BOUNDARY;
+    this._strategies = [];
+    this._strategies[0] = function(step, lowerBound, upperBound){
+        Math.seedrandom();
+        var offset = Math.floor(Math.random() * step) + 1;
+        return lowerBound + offset;
+    };
+    
+    this._strategies[1] = function(step, lowerBound, upperBound){
+        Math.seedrandom();
+        var offset = Math.floor(Math.random() * step) + 1;
+        return upperBound - offset;
+    };
+    
+    this._seed = '123456';
+    this._root = new LSEQNode(null);
+    this._root.children[0] = new LSEQNode(null);
+    this._root.children[this._base - 1] = new LSEQNode(null);
 }
 
-// pour l'instant on insère toujours au début
-// id1 et id2 sont des listes de positions, ex: [12, 45, 122];
-// on suppose que id1 < id2
-// que id1 et id2 sont corrects
-// et pas d'élément entre les id1 et id2
-// si id1.length == id2.length alors seul le dernier élément de l'id est différent
-// si id1.length != id2.length alors l'id le plus petit est commun à l'autre
-// le nouvel id sera id1 < newId < id2
-// pas de gestion des suppresions
-LSEQ.prototype.insert = function(elem, id1, id2){
-  var levelDiff = Math.abs(id1.length - id2.length);
-  var lastLevelId = 0;
-  var parentId = id1;
-  var parent;
-  
-  if((levelDiff == 0 && id2[id1.length - 1] - id1[id1.length - 1] > 1) ||
-      (levelDiff != 0 && id1[id1.length - 1] < Math.pow(id1.length - 1 + 5, 2))){
-    lastLevelId = id1[id1.length - 1] + 1;
-    parentId = id1.slice(0, id1.length - 1);
-  }
-  
-  parent = this._getNode(parentId);
-  parent.children[lastLevelId] = new LSEQNode(elem);
-  return parentId.concat(lastLevelId);
+/**
+ * \brief Inserts a new node in the tree
+ *
+ * \param prevId
+ *      ID of the node just before the new one
+ * \param value
+ *      Value of the new node
+ * \param nextId
+ *      ID of the node just after the new one
+ */
+LSEQTree.prototype.insert = function(prevId, value, nextId){
+    var depth = 0;
+    var lowerBound = prevId[depth];
+    var upperBound = nextId[depth];
+    var interval = upperBound - lowerBound - 1;
+    var parentId = this._prefix(prevId, depth - 1);
+    
+    while(interval < 1){
+        depth++;
+
+        if(prevId.length > depth && nextId.length <= depth){
+          lowerBound = prevId[depth];
+          upperBound = nextId[depth];
+          parentId = this._prefix(prevId, depth - 1);
+        }
+        else if(prevId.length > depth && nextId.length <= depth){
+          lowerBound = prevId[depth];
+          upperBound = this._maxId(depth);
+          parentId = this._prefix(prevId, depth - 1);
+        }
+        else if(prevId.length <= depth && nextId.length <= depth){
+            lowerBound = 0;
+            upperBound = this._maxId(depth);
+            parentId = this._prefix(prevId, depth - 1);
+        }
+        else{
+          if(prevId[depth - 1] != nextId[depth - 1]){
+            lowerBound = prevId[depth - 1];
+            upperBound = nextId[depth - 1] + 1;
+            parentId = this._prefix(prevId, depth - 2);
+          }
+          else{
+            var jump = nextId.length - 1 - depth;
+            depth = nextId.length - 1;
+            
+            if(nextId[depth] > 1){
+              lowerBound = 0;
+              upperBound = nextId[depth];
+              parentId = this._prefix(nextId, depth - 1);
+            }
+            else{
+              lowerBound = 0;
+              upperBound = this._maxId(depth + 1);
+              parentId = prevId.concat([0]);
+              
+              for(var i=1; i<=jump; i++){
+                parentId = parentId.concat([0]);
+              }
+            }
+          }
+        }
+        
+        interval = upperBound - lowerBound - 1;
+    }
+    
+    var step = Math.min(this._boundary, interval);
+    var strategyId = this._h(depth);
+    var lastDepthId = this._strategies[strategyId](step, lowerBound, upperBound);
+    var parent = this._getNode(parentId, function(node){
+        node.size++;
+    });
+        
+    if(parent.children[lastDepthId] === undefined){
+        parent.children[lastDepthId] = new LSEQNode(value);
+    }
+    else{
+        parent.children[lastDepthId].value = value;
+    }
+    
+    return parentId.concat(lastDepthId);
 };
 
-LSEQ.prototype._getNode = function(id){
-  var iter = this._root;
-  
-  for(var idLevel in id){
-    iter = iter.children[id[idLevel]];
-  }
-  
-  return iter;
+// return l'id pour un offset donné, ne marche pas pour l'instant
+LSEQTree.prototype.getId = function(position){
+    var id = [];
+    var iter = this._root;
+    var currentPosition = -1;
+
+    while(currentPosition != position){
+        var child = 0;
+        var branchFound = false;
+        var childIter;
+        var childPos = 0;
+            
+        while(child < iter.children.length && !branchFound){
+            childIter = iter.children[child];
+                
+            if(childIter !== undefined){
+                branchFound = currentPosition + childPos + childIter.size >= position;
+                childPos++;
+            }
+                
+            child++;
+        }
+            
+        currentPosition = currentPosition + childPos - 1;
+        iter = childIter;
+        id.push(child - 1);
+    }
+    
+    return id;
 };
 
-LSEQ.prototype.delete = function(id){
+
+LSEQTree.prototype.delete = function(id){
+    var parent = this._getNode(this._prefix(id, id.length - 2), function(node){
+        node.size--;
+    });
+    var lastDepthId = id[id.length - 1];
+    var node = parent.children[lastDepthId];
+    node.value = null;
+    
+    if(node.size == 0){
+        parent.children[lastDepthId] = null;
+    }
 };
 
-LSEQ.prototype.toString = function(){
+LSEQTree.prototype._h = function(depth){
+    Math.seedrandom(this._seed * (depth + 1));
+    return Math.round(Math.random());
+};
+
+LSEQTree.prototype._maxId = function(depth){
+    return this._base * (1 << depth);
+};
+
+// Apply function f on every node in the branch
+LSEQTree.prototype._getNode = function(id, f){
+    var fnode = f || function(node){};
+    var node = this._root;
+    fnode(node);
+  
+    for(var i=0; i<id.length; i++){        
+        if(node.children[id[i]] === undefined){
+          node.children[id[i]] = new LSEQNode(null);
+        }
+        
+        node = node.children[id[i]];
+        fnode(node);
+    }
+      
+    return node;
+};
+
+LSEQTree.prototype.size = function(){
+    return this._root.size;
+};
+
+/**
+ * \brief Tests if the tree contains an id
+ *
+ * \param id
+ *      A node id
+ * 
+ * \return true if given id matches an existing not empty node,
+ *         false otherwise
+ */
+LSEQTree.prototype._exists = function(id){
+    var depth = 1;
+    var currentNode = this._root[id[0]];
+    
+    while(currentNode !== undefined && depth < id.length){
+        currentNode = currentNode.children[id[depth]];
+        depth++;
+    }
+
+    return currentNode != undefined;
+};
+
+/**
+ * Returns given id truncated to depth.
+ */
+LSEQTree.prototype._prefix = function(id, depth){
+    return id.slice(0, depth + 1);
+};
+
+LSEQTree.prototype.toString = function(){
   return this._root.toString();
 };
 
-LSEQ.prototype.messageDelivered = function(msg){
+LSEQTree.prototype._messageDelivered = function(msg){
   console.log(msg);
 };
+
+/*var t1 = new LSEQTree();
+t1._root.children[2] = new LSEQNode('a');
+t1._root.children[3] = new LSEQNode('b');
+t1._root.children[2].children[5] = new LSEQNode('c');
+var id = t1.insert([2, 5], 'X', [3]);
+console.log(id.toString());
+console.log(t1.toString())*/
+
+/*var t2 = new LSEQTree();
+t2._root.children[2] = new LSEQNode('a');
+t2._root.children[3] = new LSEQNode('b');
+t2._root.children[2].children[63] = new LSEQNode('c');
+var id = t2.insert([2, 63], 'X', [3]);
+console.log(id.toString());
+console.log(t2.toString());*/
+
+/*var t3 = new LSEQTree();
+t3._root.children[2] = new LSEQNode('a');
+t3._root.children[4] = new LSEQNode('b');
+var id = t3.insert([2], 'X', [4]);
+console.log(id.toString());
+console.log(t3.toString());*/
+
+/*var t4 = new LSEQTree();
+t4._root.children[2] = new LSEQNode('a');
+t4._root.children[3] = new LSEQNode('b');
+t4._root.children[3].children[7] = new LSEQNode('c');
+var id = t4.insert([2], 'X', [3, 7]);
+console.log(id.toString());
+console.log(t4.toString());*/
+
+
+// tests : boucle dans les deux sens
+
+var t5 = new LSEQTree();
+
+var id = [31];
+
+for(var i=0; i<20; i++){
+    id = t5.insert([0], '|' + i, id);
+}
+
+console.log(t5.toString());
+console.log(t5.size());
--- pbcast.js
+++ pbcast.js
@@ -14,183 +14,159 @@ Pour l'instant peerjs server affecte les ids
 
 ***********************************************/
 
-function PBCast(id, r, k, server){  
-  this._peer = this._initializePeer(id, server);
-  this._joined = false;
-  this._joinId;
-  this._connections = [];
-  this._clocks = this._initializeClocks(r || this.DEFAULT_R);
-  this._entries = this._getEntries(this._peer.id, this._clocks.length, k || this.DEFAULT_K);
-  this._listeners = [];
-  this._notDelivered = [];
-  this._delivered = [];
-  this._cacheMessage = [];
-}
-
-PBCast.prototype.DEFAULT_HOST = '0.peerjs.com';
-
-PBCast.prototype.DEFAULT_PORT = 9000;
-
-PBCast.prototype.DEFAULT_R = 100;
-
-PBCast.prototype.DEFAULT_K = 5;
-
-PBCast.prototype._initializeClocks = function(r){
-  var clks = [];
-    
-  for(var i=0; i<r; i++){
-      clks.push(0);
+function PBCastBuilder(){
+  function PBCast(id, r, k, server){  
+    this._peer = this._initializePeer(id, server);
+    this._connections = [];
+    this._clocks = this._initializeClocks(r || this.DEFAULT_R);
+    this._entries = this._getEntries(this._peer.id, this._clocks.length, k || this.DEFAULT_K);
+    this._listeners = [];
+    this._notDelivered = [];
+    this._delivered = [];
   }
-    
-  return clks;
-};
 
-PBCast.prototype._initializePeer = function(id, server){
-  var peer;
-  
-  var options = {
-      key: '5g9qpwrh59v34n29',
-      host: server && server.host || this.DEFAULT_HOST,
-      port: server && server.port || this.DEFAULT_PORT
-    };
-  
-  if(id === undefined){
-    peer = new Peer(options);
-  }else{
-    peer = new Peer(id, options);
-  }
-  
-  peer.on('open', function(id){
-    var connection = peer.connect(this._joinId);
-    connection.on('open', function(){
-      this._connections.push(connection);
-      this._joined = true;
-      this._emptyCacheMessage();
-    });
-  });
-  
-  peer.on('connection', function(connection){
-    var peerId = connection.peer;
-    
-    if(this._peer.connections.peerId === undefined){
-      this._connections.push(connection);
-      // envoyer ids des peers que l'on connait
-      connection.on('data', function(message){
-	//var peerEntries = getEntries(peerId, clocks.length, entries.length);
-	
-	if(true /*|| ready(message, peerId)*/){
-	  // incrementClocks(peerEntries);
-	  deliver({error: false, msg: message.msg});
-	  // checkNoDelivered();
-	}else{
-	  this._notDelivered.push({peerId: peerId, msg: msg});
-	}
-      });
+  PBCast.prototype.DEFAULT_HOST = '0.peerjs.com';
+
+  PBCast.prototype.DEFAULT_PORT = 9000;
+
+  PBCast.prototype.DEFAULT_R = 100;
+
+  PBCast.prototype.DEFAULT_K = 5;
+
+  PBCast.prototype._initializeClocks = function(r){
+    var clks = [];
+      
+    for(var i=0; i<r; i++){
+	clks.push(0);
     }
-  });
-    
-  return peer;
-};
+      
+    return clks;
+  };
 
-PBCast.prototype._checkNoDelivered = function (){
-  var i = 0;
-  
-  while(i < this._notDelivered.length){
-    var msg = notDelivered[i];
-    var peerEntries = this._getEntries(msg.peerId, this._clocks.length, this._entries.length);
+  PBCast.prototype._initializePeer = function(id, server){
+    var peer;
     
-    if(ready(peerEntries, msg.msg.clks)){
-      msg = this._notDelivered.shift().msg;
-      this._incrementClocks(peerEntries);
-      this._deliver({error: false, msg: msg});
+    var options = {
+	key: '5g9qpwrh59v34n29',
+	host: server && server.host || this.DEFAULT_HOST,
+	port: server && server.port || this.DEFAULT_PORT
+      };
+    
+    if(id === undefined){
+      peer = new Peer(options);
     }else{
+      peer = new Peer(id, options);
+    }
+    
+    peer.on('open', function(id){
+
+      });
+    });
+    
+    peer.on('connection', function(connection){
+      var peerId = connection.peer;
+      
+      if(this._peer.connections.peerId === undefined){
+	this._connections.push(connection);
+	// envoyer ids des peers que l'on connait
+	connection.on('data', function(message){
+	  //var peerEntries = getEntries(peerId, clocks.length, entries.length);
+	  
+	  if(true /*|| ready(message, peerId)*/){
+	    // incrementClocks(peerEntries);
+	    deliver({error: false, msg: message.msg});
+	    // checkNoDelivered();
+	  }else{
+	    this._notDelivered.push({peerId: peerId, msg: msg});
+	  }
+	});
+      }
+    });
+      
+    return peer;
+  };
+
+  PBCast.prototype._checkNoDelivered = function (){
+    var i = 0;
+    
+    while(i < this._notDelivered.length){
+      var msg = notDelivered[i];
+      var peerEntries = this._getEntries(msg.peerId, this._clocks.length, this._entries.length);
+      
+      if(ready(peerEntries, msg.msg.clks)){
+	msg = this._notDelivered.shift().msg;
+	this._incrementClocks(peerEntries);
+	this._deliver({error: false, msg: msg});
+      }else{
+	i++;
+      }
+    }
+  };
+
+  PBCast.prototype._ready = function(peerEntries, clks){    
+    return checkPeerEntries(peerEntries, clks) &&
+      checkOtherEntries(peerEntries, clks);
+  };
+
+  PBCast.prototype._checkPeerEntries = function(peerEntries, msgClocks){
+    var ready = false;
+    var i = 0;
+    
+    while(!ready && i < peerEntries.length){
+      ready = clocks[i] >= msgClocks[i] - 1;
       i++;
     }
-  }
-};
+      
+    return ready;
+  };
 
-PBCast.prototype._ready = function(peerEntries, clks){    
-  return checkPeerEntries(peerEntries, clks) &&
-    checkOtherEntries(peerEntries, clks);
-};
+  PBCast.prototype._checkOtherEntries = function (peerEntries, msgClocks){
+    var ready = false;
+    var i = 0;
+    var otherEntries = []; // TODO
+      
+    while(!ready && i < otherEntries.length){
+      ready = clocks[i] >= msgClocks[i];
+      i++;
+    }
+      
+    return ready;
+  };
 
-PBCast.prototype._checkPeerEntries = function(peerEntries, msgClocks){
-  var ready = false;
-  var i = 0;
-  
-  while(!ready && i < peerEntries.length){
-    ready = clocks[i] >= msgClocks[i] - 1;
-    i++;
-  }
+  PBCast.prototype._deliver = function(message){
+    for(var listener in this._listeners){
+      this._listeners[listener].messageDelivered(message);
+    }
     
-  return ready;
-};
-
-PBCast.prototype._checkOtherEntries = function (peerEntries, msgClocks){
-  var ready = false;
-  var i = 0;
-  var otherEntries = []; // TODO
+    console.log(message.msg);
+  };
     
-  while(!ready && i < otherEntries.length){
-    ready = clocks[i] >= msgClocks[i];
-    i++;
-  }
+  PBCast.prototype._getEntries = function(id, r, k){
+    return []; // TODO
+  };
     
-  return ready;
-};
+  PBCast.prototype.addListener = function(listener){
+    this._listeners.push(listener);
+  };
+    
+  PBCast.prototype.send = function(msg){
+    if(this._joined){
+      this._incrementClocks(this._entries);
+      this._broadcast({clks: this._clocks, msg: msg});
+    }else{
+      this._cacheMessage.push(msg);
+    }
+  };
 
-PBCast.prototype._deliver = function(message){
-  for(var listener in this._listeners){
-    this._listeners[listener].messageDelivered(message);
-  }
-  
-  console.log(message.msg);
-};
-  
-PBCast.prototype._getEntries = function(id, r, k){
-  return []; // TODO
-};
-  
-PBCast.prototype.addListener = function(listener){
-  this._listeners.push(listener);
-};
+  PBCast.prototype._incrementClocks = function(entries){
+    for(var entry in entriesj){
+	this._clocks[entry]++;
+    }
+  };
 
-PBCast.prototype.join = function(id){
-  this._joinId = id;
-};
-
-PBCast.prototype._join = function(){    
-  var connection = peer.connect(joinId);
-  connection.on('open', function(){
-    this._connections.push(connection);
-    this._joined = true;
-    this._emptyCacheMessage();
-  });
-};
-  
-PBCast.prototype._emptyCacheMessage = function(){
-  while(this._cacheMessage.length > 0){
-    this.send(this._cacheMessage.shift());
-  }
-};
-  
-PBCast.prototype.send = function(msg){
-  if(this._joined){
-    this._incrementClocks(this._entries);
-    this._broadcast({clks: this._clocks, msg: msg});
-  }else{
-    this._cacheMessage.push(msg);
-  }
-};
-
-PBCast.prototype._incrementClocks = function(entries){
-  for(var entry in entriesj){
-      this._clocks[entry]++;
-  }
-};
-
-PBCast.prototype._broadcast = function(msg){
-  for(var connection in this._connections){
-    this._connections[connection].send(msg);
-  }
-};
+  PBCast.prototype._broadcast = function(msg){
+    for(var connection in this._connections){
+      this._connections[connection].send(msg);
+    }
+  };
+}
