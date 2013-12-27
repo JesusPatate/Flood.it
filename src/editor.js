8fa6d0bf8f973dfc95ed1667ea8ace517dba284e
jesuspatate <g.a.e.t.a.n@orange.fr>
2013-12-27 12:41:19 +0100

    Support for deletion of several lines

--- src/editor.js
+++ src/editor.js
@@ -11,7 +11,6 @@
 
 /**
  * \class Editor
- * \brief ...
  */
 function Editor(contentEditableId){
 	var self = this;
@@ -26,7 +25,7 @@ function Editor(contentEditableId){
 		if(!self._external){
 			var startOffset = self._editor.getSession().getDocument().positionToIndex(e.data.range.start);
 			var endOffset = self._editor.getSession().getDocument().positionToIndex(e.data.range.end);
-			
+            
 			if(startOffset == endOffset) {
 				++endOffset;
 			}
@@ -45,7 +44,23 @@ function Editor(contentEditableId){
 						self.emit("edit", {type: 'delete', offset: (startOffset + 1)});
 					}
 					break;
-                                        
+
+                case 'insertLines' :
+                	console.log("DBG " + e.data);
+                    break;
+
+                case 'removeLines' :
+                	var lines = e.data.lines;
+                    var nbLines = e.data.lines.length;
+                    var offset = startOffset;
+                    
+                    for(var i = 0 ; i < nbLines ; ++i) {
+                    	for(var j = 0 ; j < lines[i].length + 1 ; ++j) {
+                        	self.emit("edit", {type: 'delete', offset: (startOffset + 1)});
+                        }
+                    }
+                    break;
+                    
 				default :
 			}
 		}
@@ -56,15 +71,14 @@ Editor.prototype = Object.create( EventEmitter.prototype);
 Editor.prototype.constructor = Editor;
 
 /**
- * \brief ...
+ * \brief Inserts a character.
  *
  * \param value
- * 		...
+ 		Character to insert.
  * \param offset
- * 		...
+ 		Offset at which the character must be inserted.
  */
 Editor.prototype.insert = function(value, offset){
-	console.log("DBG insert: " + offset);
 	var self = this;
 	var delta = this._asDelta('insertText', offset, value);
 	this._offAir(function(){
@@ -73,13 +87,12 @@ Editor.prototype.insert = function(value, offset){
 };
 
 /**
- * \brief ...
+ * \brief Deletes a character.
  *
  * \param offset
- * 		...
+ 		Offset at which the character must be deleted.
  */
 Editor.prototype.delete = function(offset){
-	console.log("DBG delete: " + offset);
 	var self = this;
 	var delta = this._asDelta('removeText', offset);
 	this._offAir(function(){
@@ -87,18 +100,33 @@ Editor.prototype.delete = function(offset){
 	});
 };
 
-Editor.prototype._asDelta = function(op, idx, text){
+/**
+ * \brief Builds an Ace delta from LSEQ data.
+ *
+ * \param type
+ *		Type of the operation
+ * \param idx
+ * 		Offset at which the operation must executed
+ * \param text
+ * 		Text to insert (useless for deletion)
+ */
+Editor.prototype._asDelta = function(type, offset, text){
   return {
-	action: op,
+	action: type,
 	range: {
-	  start: this._editor.getSession().getDocument().indexToPosition(idx),
-	  end: this._editor.getSession().getDocument().indexToPosition(idx+1)
+	  start: this._editor.getSession().getDocument().indexToPosition(offset),
+	  end: this._editor.getSession().getDocument().indexToPosition(offset+1)
 	},
 	text: text
   };
 };
 
-// Execute a block without broadcasting the change
+/**
+ * \brief Executes a block without broadcasting the change
+ *
+ * \param block
+ *		Instructions block
+ */
 Editor.prototype._offAir = function(block){
   var was = this._external;
   this._external = true;
